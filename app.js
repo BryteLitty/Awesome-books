182dc7c64c9f631e25e634523d4ef38f30c1a595
littyofficials@gmail.com <BryteLitty>
2022-11-04 07:16:39 -0700

    Add single page functionalities

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const authorInput = document.getElementById('author');
 const form = document.querySelector('form');
 const booksContainer = document.querySelector('.books-container');
 
+const currentTime = document.querySelector('#current-time');
+const booksSection = document.getElementById('books-section');
+const addBookSection = document.getElementById('add-book-form');
+const contactSection = document.getElementById('contact');
+const navLinks = document.querySelectorAll('.nav-item');
+
+setInterval(() => {
+  currentTime.innerHTML = new Date();
+});
+
+document.addEventListener('DOMContentLoaded', displayComponents(navLinks))
+
 // books collection
 let booksCollection = JSON.parse(localStorage.getItem('books-collection')) || [];
 
@@ -41,7 +53,7 @@ function injectContent(arr) {
   booksContainer.innerHTML = books;
 }
 
-injectContent(booksCollection);
+
 
 // create book
 function createBook(book) {
@@ -94,3 +106,48 @@ booksContainer.addEventListener('click', (e) => {
   injectContent(booksCollection);
   localStorage.setItem('books-collection', JSON.stringify(booksCollection));
 });
+
+
+function displayComponents(id) {
+  switch(id) {
+    case 'books-section': 
+      booksSection.style.display = "block";
+      addBookSection.style.display = "none";
+      contactSection.style.display = "none";
+      break;
+    
+    case 'add-book-form': 
+      booksSection.style.display = "none";
+      addBookSection.style.display = "block";
+      contactSection.style.display = "none";
+      break;
+  
+    case 'contact': 
+      booksSection.style.display = "none";
+      addBookSection.style.display = "none";
+      contactSection.style.display = "block";
+      contactSection.style.backgroundColor = "red";
+      break;
+    default:
+      booksSection.style.color = "block";
+      addBookSection.style.display = "none";
+      contactSection.style.display = "none";
+  }
+};
+
+console.log(contactSection)
+
+function navItemClicked(navLinks) {
+  navLinks.forEach((item) => {
+    item.addEventListener('click', e => {
+      const id = e.target.getAttribute('displayComponent');
+      console.log(id)
+      displayComponents(id)
+    })
+});
+};
+
+
+displayComponents(navLinks)
+injectContent(booksCollection);
+navItemClicked(navLinks)
